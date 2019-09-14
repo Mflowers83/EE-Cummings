1f6f25de4d9ed114506fc3d134472f29136cd9b0
Mflowers83 <55030551+Mflowers83@users.noreply.github.com>
2019-09-14 14:58:54 -0400

    Add files via upload
    EE Cummings Projct

---
+++ EE_Cummings.java
@@ -0,0 +1,29 @@
+public class EE Cummings{
+	public static void main(String[]args){
+            
+        System.out.println("O'malley says, \"Practice \\ makes \\ perfect! \t");
+        System.out.println("Now, \\\\ \"without further adeiu, a poem inside a frame \: \n \n");
+        System.out.println(" \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ");
+        System.out.println("\@");
+        System.out.println("\@ \t \t r-p-o-p-h-e-s-s-a-g-r");
+        System.out.println("\@ \t \t E.E. Cummings - 1894-1962 \n \n \n");
+        System.out.println("\@ \t \t \t r-p-o-p-h-e-s-s-a-g-r");
+        System.out.println("\@ \t \t \t who");
+        System.out.println("\@ a\)s w\(e loo\)k");
+        System.out.println("\@ upnowgath");
+        System.out.println("\@ \t \t PPEGORHRASS");
+        System.out.println("\@ \t \t \t \t eringint \( o-");
+        System.out.println("\@ aThe \) 1");
+        System.out.println("\@ \t \t eA");
+        System.out.println("\@ \t \t \t \t !p:");
+        System.out.println("\@ S \t \t \t \t \t a");
+        System.out.println("\@ \t \t \t \( r");
+        System.out.println("\@ rIvInG \t \t .gRrEaPsPh0s \)");
+        System.out.println("\@ \t \t \t \t \t to");
+        System.out.println("\@ rea \( be \) rran \( com \) gi \( e \) ngly");
+        System.out.println("\@ ,grasshopper \;");
+        System.out.println("\@");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");"
+                                                                                                                
+    }
+}
